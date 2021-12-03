7648600dd6fa569725ef9215c5b372f69e13f6b6
Andreas Runfalk <andreas@runfalk.se>
2021-12-04 00:51:46 +0100

    Simplified argument parsing using structopt

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,47 @@ use advent_of_code_2021::day1;
 use advent_of_code_2021::day2;
 use advent_of_code_2021::day3;
 use anyhow::{anyhow, Result};
-use std::path::Path;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    /// The day to run the solution for (1-25)
+    day: usize,
+
+    /// The input data file. Will look for `data/day<num>.txt` by default
+    input: Option<PathBuf>,
+}
 
 fn pad_newlines(answer: String) -> String {
     answer.lines().collect::<Vec<_>>().join("\n   ")
 }
 
-fn as_result<A: ToString, B: ToString>(value: (A, Option<B>)) -> (String, Option<String>) {
-    (
-        value.0.to_string(),
-        value.1.map(|answer| answer.to_string()),
-    )
+fn as_result<A: ToString, B: ToString>((a, b): (A, Option<B>)) -> (String, Option<String>) {
+    (a.to_string(), b.map(|answer| answer.to_string()))
 }
 
 fn main() -> Result<()> {
-    let args: Vec<_> = std::env::args().collect();
+    let opts = Options::from_args();
+    let input = opts
+        .input
+        .unwrap_or_else(|| format!("data/day{}.txt", opts.day).into());
 
-    if args.len() < 2 {
-        return Err(anyhow!("Not enough arguments"));
-    }
-
-    let path: Option<&Path> = if args.len() == 3 {
-        Some(Path::new(&args[2]))
-    } else {
-        None
+    #[allow(
+        overlapping_range_endpoints,
+        unreachable_patterns,
+        clippy::match_overlapping_arm
+    )]
+    let (a, b): (String, Option<String>) = match opts.day {
+        1 => as_result(day1::main(&input)?),
+        2 => as_result(day2::main(&input)?),
+        3 => as_result(day3::main(&input)?),
+        1..=25 => return Err(anyhow!("No implementation for this day yet")),
+        day => return Err(anyhow!("Day {} is not a valid day for advent of code", day)),
     };
 
-    #[allow(overlapping_range_endpoints, unreachable_patterns)]
-    let result: (String, Option<String>) = match args[1].parse() {
-        Ok(1) => as_result(day1::main(
-            path.unwrap_or_else(|| Path::new("data/day1.txt")),
-        )?),
-        Ok(2) => as_result(day2::main(
-            path.unwrap_or_else(|| Path::new("data/day2.txt")),
-        )?),
-        Ok(3) => as_result(day3::main(
-            path.unwrap_or_else(|| Path::new("data/day3.txt")),
-        )?),
-        Ok(1..=25) => return Err(anyhow!("No implementation for this day yet")),
-        Ok(day) => return Err(anyhow!("Day {} is not a valid day for advent of code", day)),
-        Err(_) => return Err(anyhow!("{:?} is not a valid day", args[1])),
-    };
-
-    println!("A: {}", pad_newlines(result.0));
-    if let Some(b) = result.1 {
+    println!("A: {}", pad_newlines(a));
+    if let Some(b) = b {
         println!("B: {}", pad_newlines(b));
     }
 
