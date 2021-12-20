bff49933272dc34add33227348e9dfe67a434225
Andreas Runfalk <andreas@runfalk.se>
2021-12-21 00:45:01 +0100

    Shorten day 20 solution by removing Coordinate class

--- src/day20.rs
+++ src/day20.rs
@@ -2,79 +2,57 @@ use anyhow::{anyhow, Result};
 use std::collections::HashSet;
 use std::path::Path;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-struct Coordinate {
-    x: isize,
-    y: isize,
-}
-
-impl Coordinate {
-    fn new(x: isize, y: isize) -> Self {
-        Self { x, y }
-    }
-
-    fn iter_3x3(self) -> impl Iterator<Item = Self> {
-        [
-            Coordinate::new(self.x - 1, self.y - 1),
-            Coordinate::new(self.x, self.y - 1),
-            Coordinate::new(self.x + 1, self.y - 1),
-            Coordinate::new(self.x - 1, self.y),
-            Coordinate::new(self.x, self.y),
-            Coordinate::new(self.x + 1, self.y),
-            Coordinate::new(self.x - 1, self.y + 1),
-            Coordinate::new(self.x, self.y + 1),
-            Coordinate::new(self.x + 1, self.y + 1),
-        ]
-        .into_iter()
-    }
-}
-
 struct SparseImage {
     min_x: isize,
     max_x: isize,
     min_y: isize,
     max_y: isize,
     rest_is_light: bool,
-    light_pixels: HashSet<Coordinate>,
+    light_pixels: HashSet<(isize, isize)>,
 }
 
 impl SparseImage {
-    fn new(light_pixels: HashSet<Coordinate>) -> Self {
+    fn new(light_pixels: HashSet<(isize, isize)>) -> Self {
         Self {
-            min_x: light_pixels.iter().copied().map(|c| c.x).min().unwrap_or(0),
-            max_x: light_pixels.iter().copied().map(|c| c.x).max().unwrap_or(0),
-            min_y: light_pixels.iter().copied().map(|c| c.y).min().unwrap_or(0),
-            max_y: light_pixels.iter().copied().map(|c| c.y).max().unwrap_or(0),
+            min_x: light_pixels.iter().map(|(x, _)| *x).min().unwrap_or(0),
+            max_x: light_pixels.iter().map(|(x, _)| *x).max().unwrap_or(0),
+            min_y: light_pixels.iter().map(|(_, y)| *y).min().unwrap_or(0),
+            max_y: light_pixels.iter().map(|(_, y)| *y).max().unwrap_or(0),
             rest_is_light: false,
             light_pixels,
         }
     }
 
-    fn is_light(&self, c: Coordinate) -> bool {
-        if (self.min_x..=self.max_x).contains(&c.x) && (self.min_y..=self.max_y).contains(&c.y) {
-            self.light_pixels.contains(&c)
+    fn is_light(&self, (x, y): (isize, isize)) -> bool {
+        if (self.min_x..=self.max_x).contains(&x) && (self.min_y..=self.max_y).contains(&y) {
+            self.light_pixels.contains(&(x, y))
         } else {
             self.rest_is_light
         }
     }
 
-    fn enhance(&mut self, iea: &[bool; 512]) {
+    fn enhance(&mut self, image_enhancement_algorithm: &[bool; 512]) {
         let mut light_pixels = HashSet::new();
 
         // We search an area just outside the image as well since the pixels inside the current
         // image may affect them
         let image_coordinates = (self.min_y - 1..=self.max_y + 1)
-            .flat_map(|y| (self.min_x - 1..=self.max_x + 1).map(move |x| Coordinate::new(x, y)));
-        for c in image_coordinates {
+            .flat_map(|y| (self.min_x - 1..=self.max_x + 1).map(move |x| (x, y)));
+        for (x, y) in image_coordinates {
             // Find the correct lookup location by converting the area around the pixel to an
             // integer that we use to lookup the correct location in the image enhancement algorithm
-            let i: usize = c
-                .iter_3x3()
-                .enumerate()
-                .filter(|(_, c)| self.is_light(*c))
-                .fold(0usize, |i, (b, _)| i | 1 << (8 - b));
-            if iea[i] {
-                light_pixels.insert(c);
+            let mut index = 0;
+            let mut bit = 8;
+            for ny in y - 1..=y + 1 {
+                for nx in x - 1..=x + 1 {
+                    if self.is_light((nx, ny)) {
+                        index |= 1 << bit;
+                    }
+                    bit -= 1;
+                }
+            }
+            if image_enhancement_algorithm[index] {
+                light_pixels.insert((x, y));
             }
         }
 
@@ -82,9 +60,9 @@ impl SparseImage {
 
         // The rest of the pixels may or may not toggle based on the enhancement algorithm
         if self.rest_is_light {
-            self.rest_is_light = iea[511];
+            self.rest_is_light = image_enhancement_algorithm[511];
         } else {
-            self.rest_is_light = iea[0];
+            self.rest_is_light = image_enhancement_algorithm[0];
         }
 
         // Since we have checked pixels just outside the current image we must expand the image
@@ -102,7 +80,7 @@ pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
         .split_once("\n\n")
         .ok_or_else(|| anyhow!("Invalid input"))?;
 
-    let iea: [bool; 512] = enhancement_str
+    let image_enhancement_algorithm: [bool; 512] = enhancement_str
         .chars()
         .map(|c| match c {
             '#' => Ok(true),
@@ -122,7 +100,7 @@ pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
         .flat_map(|(y, l)| {
             l.chars().enumerate().filter_map(move |(x, c)| {
                 if c == '#' {
-                    Some(Coordinate::new(x as isize, y as isize))
+                    Some((x as isize, y as isize))
                 } else {
                     None
                 }
@@ -132,12 +110,12 @@ pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
 
     let mut image = SparseImage::new(light_pixels);
     for _ in 0..2 {
-        image.enhance(&iea);
+        image.enhance(&image_enhancement_algorithm);
     }
     let a = image.light_pixels.len();
 
     for _ in 2..50 {
-        image.enhance(&iea);
+        image.enhance(&image_enhancement_algorithm);
     }
     let b = image.light_pixels.len();
 
